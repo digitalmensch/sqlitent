90eacdd1a23e1a0be401b380a3a93f6a8bd5eeb3
TA <tag@adnm.ch>
2018-01-06 12:09:15 +0000

    Rename various things

--- sqlitent.py
+++ sqlitent.py
@@ -23,32 +23,32 @@ class sqlitent(collections.abc.Collection):
                    hasattr(nt, '_source') and hasattr(nt, '_replace') and \
                    hasattr(nt, '_asdict')
 
-    def __getsqlname(self, name):
+    def __sqlname(self, name):
         # SQL is case insensitive. Append the hex encoded value for uniqueness.
         unique = name.encode('ascii').hex()
         return f'{name}_{unique}'
 
-    def __getsqltype(self, _type):
-        if int   == _type: return 'INTEGER'
-        if float == _type: return 'REAL'
-        if str   == _type: return 'TEXT'
-        if bytes == _type: return 'BLOB'
+    def __sqltype(self, nt_type):
+        if int   == nt_type: return 'INTEGER'
+        if float == nt_type: return 'REAL'
+        if str   == nt_type: return 'TEXT'
+        if bytes == nt_type: return 'BLOB'
         return ''
 
-    def __gettablename(self, nt):
-        return self.__getsqlname(type(nt).__name__)
+    def __tablename(self, nt):
+        return self.__sqlname(type(nt).__name__)
 
     def __execute(self, stmt, *args, **kwargs):
         return self.__db.cursor().execute(stmt, *args, **kwargs)
 
     def __setuptable(self, nt):
-        table = self.__gettablename(nt)
-        _type = type(nt)
-        fields = ','.join(self.__getsqlname(field) for field in nt._fields)
+        table = self.__tablename(nt)
+        nt_type = type(nt)
+        fields = ','.join(self.__sqlname(field) for field in nt._fields)
 
         # build and run the create table statement
         stmt = ','.join(
-            f'{self.__getsqlname(f)} {self.__getsqltype(type(getattr(nt, f)))}'
+            f'{self.__sqlname(f)} {self.__sqltype(type(getattr(nt, f)))}'
             for f in nt._fields
         )
         stmt = f'CREATE TABLE IF NOT EXISTS {table} ({stmt}, UNIQUE ({fields}));'
@@ -58,23 +58,23 @@ class sqlitent(collections.abc.Collection):
         # build and cache the insert statement
         stmt = f'INSERT OR IGNORE INTO {table} ({fields})'
         stmt += 'VALUES (' + ','.join(['?'] * len(nt)) + ');'
-        self.__insert_cache[_type] = stmt
+        self.__insert_cache[nt_type] = stmt
 
         # build and cache the select statement for all tuples
-        self.__select_cache[_type] = f'SELECT {fields} FROM {table};'
+        self.__select_cache[nt_type] = f'SELECT {fields} FROM {table};'
 
         # build and cache the select statement for a fully specified tuple
-        stmt = ' AND '.join(f'{self.__getsqlname(f)} IS ?' for f in nt._fields)
+        stmt = ' AND '.join(f'{self.__sqlname(f)} IS ?' for f in nt._fields)
         stmt = f'SELECT {fields} FROM {table} WHERE {stmt};'
-        self.__single_cache[_type] = stmt
+        self.__single_cache[nt_type] = stmt
 
         # build and cache the select count(*) statement
-        self.__count_cache[_type] = f'SELECT count(*) FROM {table};'
+        self.__count_cache[nt_type] = f'SELECT count(*) FROM {table};'
 
         # build and cache the delete statement for a fully specified tuple
-        stmt = ' AND '.join(f'{self.__getsqlname(f)} IS ?' for f in nt._fields)
+        stmt = ' AND '.join(f'{self.__sqlname(f)} IS ?' for f in nt._fields)
         stmt = f'DELETE FROM {table} WHERE {stmt};'
-        self.__delete_cache[_type] = stmt
+        self.__delete_cache[nt_type] = stmt
 
     def __to_ntlist(self, it):
         def _flatten(it):
@@ -96,10 +96,10 @@ class sqlitent(collections.abc.Collection):
     def __contains__(self, nt):
         if not self.__isnamedtuple(nt):
             raise Exception(f'expected namedtuple, instead got {type(nt)}: {nt}')
-        _type = type(nt)
-        if _type not in self.__insert_cache:
+        nt_type = type(nt)
+        if nt_type not in self.__insert_cache:
             self.__setuptable(nt)
-        return bool(list(self.__execute(self.__single_cache[_type], nt)))
+        return bool(list(self.__execute(self.__single_cache[nt_type], nt)))
 
     def __iter__(self):
         return itertools.chain.from_iterable(
@@ -115,13 +115,13 @@ class sqlitent(collections.abc.Collection):
     def add(self, nt):
         if not self.__isnamedtuple(nt):
             raise Exception(f'expected namedtuple, instead got {type(nt)}: {nt}')
-        _type = type(nt)
-        if _type not in self.__insert_cache:
+        nt_type = type(nt)
+        if nt_type not in self.__insert_cache:
             self.__setuptable(nt)
         if None in nt and nt in self:
             # abort if exists, because NULL doesn't violate uniqueness in Sqlite
             return
-        self.__execute(self.__insert_cache[_type], nt)
+        self.__execute(self.__insert_cache[nt_type], nt)
         self.__db.commit()
 
     def insert(self, *nts):
@@ -132,9 +132,9 @@ class sqlitent(collections.abc.Collection):
     def remove(self, nt):
         if not self.__isnamedtuple(nt):
             raise Exception(f'expected namedtuple, instead got {type(nt)}: {nt}')
-        _type = type(nt)
-        if _type in self.__delete_cache:
-            self.__execute(self.__delete_cache[_type], nt)
+        nt_type = type(nt)
+        if nt_type in self.__delete_cache:
+            self.__execute(self.__delete_cache[nt_type], nt)
             self.__db.commit()
 
     def delete(self, *nts):
@@ -142,20 +142,20 @@ class sqlitent(collections.abc.Collection):
         for nt in tmp:
             self.remove(nt)
 
-    def one(self, _type, **kwargs):
-        for nt in self.many(_type, **kwargs):
+    def one(self, nt_type, **kwargs):
+        for nt in self.many(nt_type, **kwargs):
             return nt
         return None
 
-    def pop(self, _type, **kwargs):
-        tmp = self.one(_type, **kwargs)
+    def pop(self, nt_type, **kwargs):
+        tmp = self.one(nt_type, **kwargs)
         if tmp is not None:
             self.remove(tmp)
         return tmp
 
-    def many(self, _type, **kwargs):
-        if not all(k in _type._fields for k in kwargs):
-            raise Exception(f'{_type} doesn\'t have one of your keywords')
+    def many(self, nt_type, **kwargs):
+        if not all(k in nt_type._fields for k in kwargs):
+            raise Exception(f'{nt_type} doesn\'t have one of your keywords')
 
         clauses = []
         sqlparams = []
@@ -164,15 +164,15 @@ class sqlitent(collections.abc.Collection):
             if isinstance(value, types.FunctionType):
                 filters.append(lambda t: value(getattr(t, field)))
             else:
-                clauses.append(f'{self.__getsqlname(field)} IS ?')
+                clauses.append(f'{self.__sqlname(field)} IS ?')
                 sqlparams.append(value)
 
-        table = self.__getsqlname(_type.__name__)
+        table = self.__sqlname(nt_type.__name__)
         stmt = f'SELECT * FROM {table}' + (' WHERE ' + ' AND '.join(clauses) if clauses else '') + ';'
 
         it = self.__execute(stmt, sqlparams)
         print(it)
-        it = map(_type._make, it)
+        it = map(nt_type._make, it)
         print(it)
         for fn in filters:
             it = filter(fn, it)
@@ -180,7 +180,7 @@ class sqlitent(collections.abc.Collection):
 
         yield from it
 
-    def popmany(self, _type, **kwargs):
-        tmp = list(self.many(_type, **kwargs))
+    def popmany(self, nt_type, **kwargs):
+        tmp = list(self.many(nt_type, **kwargs))
         self.delete(tmp)
         return tmp
